4dc5510f75cc3393349943cd3975c23a438b54a7
Maria Vechtomova <130861802+mvechtomova@users.noreply.github.com>
2024-10-22 10:04:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 with open('project_config.yml', 'r') as file:
     config = yaml.safe_load(file)
 
-print("Configuration loaded:")
+logger.info("Configuration loaded:")
 print(yaml.dump(config, default_flow_style=False))
 
 
@@ -30,9 +30,6 @@ logger.info("Data preprocessed.")
 X_train, X_test, y_train, y_test = data_processor.split_data()
 logger.info(f"Data split into training and test sets.")
 logger.debug(f"Training set shape: {X_train.shape}, Test set shape: {X_test.shape}")
-
-print("Training set shape:", X_train.shape)
-print("Test set shape:", X_test.shape)
 
 # Initialize and train the model
 model = PriceModel(data_processor.preprocessor, config)
